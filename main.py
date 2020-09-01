f895f806ccf3441e5f3fe56cf3e4f84ac1bb0811
neitro <admin@rnd2.ru>
2020-09-01 21:08:09 +0300

    Replaced a list with a generator

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import time
-from typing import List, Dict
+from typing import Dict, Iterable
 """
 
 First: Run the following code using Python7+ (otherwise the type hints
@@ -10,20 +10,24 @@ Second: Figure out what the function `solution` is doing
 Third: Optimize the function `solution` to run under 2 seconds.
 It is possible to run under a second.
 
+### 
+Addition:
+Optimize work with memory:
+1) reading data from file
+
 """
 
 
-def solution(source_list: list):
-    meeting_pairs = get_pair_from_list(source_list)
+def solution(source_data: list):
+    meeting_pairs = get_pair_from_list(source_data)
     distance = get_max_distance_from_pairs(meeting_pairs)
     return distance
 
 
-def get_pair_from_list(source_list: List[int]) -> Dict[int, Dict]:
+def get_pair_from_list(source_data: Iterable) -> Dict[int, Dict]:
     # first step - get pair (first meeting, last meeting)
     meeting_pairs = dict()
-    for i in range(len(source_list)):
-        key = source_list[i]
+    for i, key in enumerate(source_data):
         if key not in meeting_pairs:
             meeting_pairs[key] = {'first': i, 'last': None}
         else:
@@ -42,12 +46,11 @@ def get_max_distance_from_pairs(pairs:  Dict[int, Dict]) -> int:
     return result
 
 
-def get_data_list_from_file(file_path: str) -> List[int]:
-    with open(file_path, "r") as file:
-        rand_array = file.read().split("\n")
-        rand_array.pop()
-        fixed_arr = list(map(int, rand_array))
-    return fixed_arr
+def get_data_list_from_file(file_path: str) -> Iterable:
+    with open(file_path, "r") as file_with_data:
+        for el in file_with_data:
+            if el.strip().isnumeric():
+                yield int(el)
 
 
 if __name__ == "__main__":
