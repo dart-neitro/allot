a6dda4e1a0e56a03fe457f1688f1cd11712b470f
neitro <admin@rnd2.ru>
2020-09-01 20:58:20 +0300

    Refactored the "solution" function

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import time
+from typing import List, Dict
 """
 
 First: Run the following code using Python7+ (otherwise the type hints
@@ -12,26 +13,32 @@ It is possible to run under a second.
 """
 
 
-def solution(my_list: list):
-    len_list = len(my_list)
+def solution(source_list: list):
+    meeting_pairs = get_pair_from_list(source_list)
+    distance = get_max_distance_from_pairs(meeting_pairs)
+    return distance
 
+
+def get_pair_from_list(source_list: List[int]) -> Dict[int, Dict]:
     # first step - get pair (first meeting, last meeting)
     meeting_pairs = dict()
-    for i in range(len_list):
-        key = my_list[i]
+    for i in range(len(source_list)):
+        key = source_list[i]
         if key not in meeting_pairs:
             meeting_pairs[key] = {'first': i, 'last': None}
         else:
             meeting_pairs[key]['last'] = i
+    return meeting_pairs
 
+
+def get_max_distance_from_pairs(pairs:  Dict[int, Dict]) -> int:
     # second step - get the max difference
     result = 0
-    for key in meeting_pairs:
-        data = meeting_pairs[key]
+    for key in pairs:
+        data = pairs[key]
         if data['last'] is not None:
             curr_dist = data['last'] - data['first']
             result = max(result, curr_dist)
-
     return result
 
 
